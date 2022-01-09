1822fbad411da17e6a89315fc227614074a2826c
Conor Finegan <conor_finegan@student.uml.edu>
2022-01-08 20:08:40 -0500

    client: argument parsing

--- client/main.js
+++ client/main.js
@@ -1,5 +1,54 @@
 import WebSocket from "ws";
 
+// first 2 arguments will always be 'path/to/node' and 'main.js'
+const args = process.argv.slice(2);
+
+function usage() {
+  console.error("usage: node path/to/main.js <type> <a> <b>");
+  console.error("<type> must be 'add' or 'subtract'");
+  console.error("<a> and <b> must be numbers");
+}
+
+// entry point
 (function() {
-  console.log("hello world!");
+  if (args.length < 3) {
+    usage();
+    return;
+  }
+
+  const type = args[0];
+  const aStr = args[1]
+  const bStr = args[2];
+
+  const a = Number(aStr);
+  const b = Number(bStr);
+
+  if (type !== "add" && type !== "subtract") {
+    console.error("Error! Invalid message type: " + type);
+    usage();
+    return;
+  }
+  if (isNaN(a)) {
+    console.error("Error! Invalid number for 'a': " + aStr);
+    usage();
+    return;
+  }
+  if (isNaN(b)) {
+    console.error("Error! Invalid number for 'b': " + bStr);
+    usage();
+    return;
+  }
+
+  const message = JSON.stringify({
+    type: type,
+    payload: {
+      a: a,
+      b: b
+    }
+  });
+
+  console.log(message);
+
+  // const connection = new WebSocket("wss://localhost:8080");
+  
 }());
